17372245f979fdb9dfc29551f294c043c1ca3203
Panupong Tipjoi <itstoon.panupong@gmail.com>
2023-06-24 06:52:01 +0700

    feat: Added description for env type

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,6 +1,13 @@
 /// <reference types="vite/client" />
 
 interface ImportMetaEnv {
+  /**
+   * App configuration
+   */
+
   readonly VITE_APP_VERSION: string
-  // more env variables...
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv
 }
