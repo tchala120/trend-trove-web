cc9b8f3adb2300ad165aacf609b0503628e1142a
Panupong Tipjoi <itstoon.panupong@gmail.com>
2023-06-24 06:53:55 +0700

    feat: Added simple query with react query custom hook from graphql codegen

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,7 +1,22 @@
+import { useListProductsQuery } from 'graphQL/__generated/operations'
+
 export const HomePage = () => {
+  const listProductsQuery = useListProductsQuery({
+    limit: 10,
+    skip: 0,
+  })
+
+  const data = listProductsQuery.data
+
+  if (listProductsQuery.isLoading) {
+    return <h1>Loading...</h1>
+  }
+
   return (
     <div>
       <h1>Here is the home page</h1>
+
+      <pre>{JSON.stringify(data, null, 2)}</pre>
     </div>
   )
 }
