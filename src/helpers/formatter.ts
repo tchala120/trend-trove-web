86f50db01e86dfbe67c49ddf1a93e8f1ed8b7427
Panupong Tipjoi <itstoon.panupong@gmail.com>
2023-06-26 00:48:35 +0700

    feat: Added money format helper

--- src/helpers/formatter.ts
+++ src/helpers/formatter.ts
@@ -1,4 +1,14 @@
 import numeral from 'numeral'
 
-export const numberFormat = (text?: number | string, format = '0,0.00') =>
+export interface MoneyFormatOptions {
+  prefix?: '$' | '฿'
+  format?: string
+}
+
+export const numberFormat = (text?: number | string, format = '0,0') =>
   numeral(text).format(format)
+
+export const moneyFormat = (
+  text?: number | string,
+  options?: MoneyFormatOptions
+) => `${options?.prefix}${numberFormat(text, options?.format || '0,0.00')}`
