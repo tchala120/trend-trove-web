18638827211c5983a2514382c28a7481bc753a20
Panupong Tipjoi <itstoon.panupong@gmail.com>
2023-06-25 21:57:03 +0700

    feat: Added not found content component

---
+++ src/components/NotFoundContent.tsx
@@ -0,0 +1,34 @@
+import { type ReactNode } from 'react'
+import styled from '@emotion/styled'
+
+interface NotFoundContentProps {
+  icon: ReactNode
+  title: ReactNode
+}
+
+export const NotFoundContent = ({ icon, title }: NotFoundContentProps) => {
+  return (
+    <NotFoundContentContainer>
+      {icon}
+
+      <NotFoundTitle>{title}</NotFoundTitle>
+    </NotFoundContentContainer>
+  )
+}
+
+const NotFoundContentContainer = styled.div`
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  min-height: 50vh;
+  gap: 24px;
+  color: #ccc;
+  text-align: center;
+`
+
+const NotFoundTitle = styled.span`
+  font-size: 24px;
+  font-weight: bold;
+`
