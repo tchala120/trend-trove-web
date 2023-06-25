1dca7be58eb822bf3e81ba907e106b791bbc3c57
Panupong Tipjoi <itstoon.panupong@gmail.com>
2023-06-26 02:13:32 +0700

    feat: Added pagination for api custom hoook

---
+++ src/hooks/usePaginationForAPI.ts
@@ -0,0 +1,38 @@
+import { useCallback, useState } from 'react'
+import merge from 'lodash.merge'
+
+interface PaginationForAPIOptions {
+  limit?: number
+  skip?: number
+  skipInterval?: number
+}
+
+const defaultPaginationForAPIOptions = {
+  limit: 10,
+  skip: 0,
+  skipInterval: 10,
+}
+
+export const usePaginationForAPI = (options?: PaginationForAPIOptions) => {
+  const opts = merge({}, defaultPaginationForAPIOptions, options)
+
+  const [skip, setSkip] = useState<number>(opts.skip)
+
+  const loadMoreContents = useCallback(
+    (totalItems = 0) => {
+      return totalItems > skip
+    },
+    [skip]
+  )
+
+  const onInfiniteLoadChange = useCallback(() => {
+    setSkip((prev) => prev + opts.skipInterval)
+  }, [opts.skipInterval])
+
+  return {
+    skip,
+    limit: opts.limit,
+    loadMoreContents,
+    onInfiniteLoadChange,
+  }
+}
