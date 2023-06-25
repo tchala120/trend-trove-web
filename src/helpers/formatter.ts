3f72dc06a7347bd913884c11694c6ba972968f33
Panupong Tipjoi <itstoon.panupong@gmail.com>
2023-06-25 21:57:24 +0700

    fix: Renamed formatter handler into `numberFormat` instead

--- src/helpers/formatter.ts
+++ src/helpers/formatter.ts
@@ -1,4 +1,4 @@
 import numeral from 'numeral'
 
-export const moneyFormat = (text: number | string, format = '0,0') =>
+export const numberFormat = (text?: number | string, format = '0,0') =>
   numeral(text).format(format)
