784cb8721708ab99cdcace5cf0735f8390fa90e5
Panupong Tipjoi <itstoon.panupong@gmail.com>
2023-06-25 15:05:20 +0700

    feat: Added api config type

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -6,6 +6,12 @@ interface ImportMetaEnv {
    */
 
   readonly VITE_APP_VERSION: string
+
+  /**
+   * API configuration
+   */
+  VITE_TREND_TROVE_GRAPHQL_URL: string
+  VITE_AUTHORIZATION_KEY: string
 }
 
 interface ImportMeta {
